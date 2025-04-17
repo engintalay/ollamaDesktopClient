d9334606276a0333d3a1fd267e5a1308b436ec59
Engin Talay <etalay@gelirler.gov.tr>
2025-04-17 21:53:29 +0300

    Jar yapacak şekilde düzenleme yapıldı

--- run.cmd
+++ run.cmd
@@ -1,3 +1,5 @@
 del *.class
 javac -encoding UTF-8 -cp .;json-20250107.jar ollama.java
-java -cp .;json-20250107.jar ollama
+jar cfe ollama.jar ollama *.class json-20250107.jar
+del *.class
+java -jar ollama.jar
