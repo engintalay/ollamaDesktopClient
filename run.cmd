5ba8d7f5f6b655af81c795b64a700dbe6d45ace4
Engin Talay <etalay@gelirler.gov.tr>
2025-04-17 22:26:00 +0300

    add

--- run.cmd
+++ run.cmd
@@ -1,4 +1,6 @@
+rmdir /s /q temp
 javac -encoding UTF-8 -cp .;json-20250107.jar;gson-2.8.9.jar ollama.java
+jar cfe ollama.jar ollama *.class 
 mkdir temp && cd temp && jar xf ../gson-2.8.9.jar && jar xf ../json-20250107.jar && jar xf ../ollama.jar && jar cfe ../ollama-fat.jar ollama * && cd .. && rmdir /s /q temp
 del *.class
 java -jar ollama-fat.jar
