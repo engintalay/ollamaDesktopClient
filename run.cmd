1de12198a83e6805770cb08c05427d758db1c19e
Engin Talay <etalay@gelirler.gov.tr>
2025-04-17 22:28:08 +0300

    compile ve sadece run scriptleri eklendi.

--- run.cmd
+++ run.cmd
@@ -1,6 +1,10 @@
-rmdir /s /q temp
-javac -encoding UTF-8 -cp .;json-20250107.jar;gson-2.8.9.jar ollama.java
-jar cfe ollama.jar ollama *.class 
-mkdir temp && cd temp && jar xf ../gson-2.8.9.jar && jar xf ../json-20250107.jar && jar xf ../ollama.jar && jar cfe ../ollama-fat.jar ollama * && cd .. && rmdir /s /q temp
-del *.class
+@echo off
+@chcp -q -- "%~dp0" "%~dpnx0" >nul 2>&1
+@chcp 65001 >nul 2>&1
+if not exist "ollama-fat.jar" (
+    echo "ollama-fat.jar bulunamadı, compile.cmd çalıştırılıyor..."
+    call compile.cmd
+) else (
+    echo "ollama-fat.jar bulundu, çalıştırılıyor..."
+)
 java -jar ollama-fat.jar
